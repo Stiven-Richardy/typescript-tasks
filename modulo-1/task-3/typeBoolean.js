e01ba6d5ae9a1b50961a2fafddc415e624727c6d
Stiven-Richardy <stivenrichardy445@gmail.com>
2025-02-10 16:58:35 -0300

    Terceira task

---
+++ modulo-1/task-3/typeBoolean.js
@@ -0,0 +1,24 @@
+"use strict";
+//Boolean - Exemplo1
+let tarefaConcluida = true;
+let tarefaPendente = false;
+console.log(tarefaConcluida);
+console.log(tarefaPendente);
+//Boolean - Exemplo2
+let tarefa = true;
+if (tarefa) {
+    console.log('Tarefa concluída');
+}
+else {
+    console.log('Tarefa pendente');
+}
+//Boolean - Exemplo3
+let idade = 18;
+let verificaIdade;
+if (idade >= 18) {
+    verificaIdade = true;
+}
+else {
+    verificaIdade = false;
+}
+console.log('É maior de idade?', verificaIdade);
---
+++ modulo-1/task-3/typeBoolean.ts
@@ -0,0 +1,27 @@
+//Boolean - Exemplo1
+let tarefaConcluida: boolean = true;
+let tarefaPendente: boolean = false;
+
+console.log(tarefaConcluida);
+console.log(tarefaPendente);
+
+//Boolean - Exemplo2
+let tarefa: boolean = true;
+
+if (tarefa) {
+    console.log('Tarefa concluída');
+} else {
+    console.log('Tarefa pendente');
+}
+
+//Boolean - Exemplo3
+let idade: number = 18;
+let verificaIdade: boolean;
+
+if (idade >= 18) {
+    verificaIdade = true;
+} else {
+    verificaIdade = false;
+}
+
+console.log('É maior de idade?', verificaIdade);
