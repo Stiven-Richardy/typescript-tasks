e586b83aa7832eb2f84460bd350794379cbf352a
Stiven-Richardy <stivenrichardy445@gmail.com>
2025-02-10 18:44:12 -0300

    Quinta task

---
+++ modulo-1/task-5/stringType.js
@@ -0,0 +1,10 @@
+"use strict";
+// -> String: Aspas simples
+let nomeCompleto = 'Stiven Richardy';
+console.log(nomeCompleto);
+// -> String: Aspas duplas
+let nomeEmpresa = "Meta Globaltech";
+console.log(nomeEmpresa);
+// -> String: Template literals
+let templateLiterals = `Funcionário: ${nomeCompleto}, Empresa: ${nomeEmpresa}`;
+console.log(templateLiterals);
---
+++ modulo-1/task-5/stringType.ts
@@ -0,0 +1,11 @@
+// -> String: Aspas simples
+let nomeCompleto: string = 'Stiven Richardy';
+console.log(nomeCompleto);
+
+// -> String: Aspas duplas
+let nomeEmpresa: string = "Meta Globaltech";
+console.log(nomeEmpresa);
+
+// -> String: Template literals
+let templateLiterals: string = `Funcionário: ${nomeCompleto}, Empresa: ${nomeEmpresa}`;
+console.log(templateLiterals);
