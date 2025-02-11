651442607d1d79f05a98ecc034686268a9f5f4f5
Stiven-Richardy <stivenrichardy445@gmail.com>
2025-02-11 12:32:58 -0300

    Atualização após a conclusão da Tarefa 12

---
+++ modulo-01-Types/task-12-Null-Undefined/nullUndefinedType.ts
@@ -0,0 +1,15 @@
+// -> Exemplo do uso do Null
+let variavelNull = null;
+
+console.log(variavelNull);
+console.log(typeof variavelNull);
+
+// -> Exemplo do uso do Undefined
+let variavelUndefined;
+
+console.log(variavelUndefined);
+console.log(typeof variavelUndefined);
+
+// -> Diferenças do Null e Undefined
+console.log('Exemplo 1:', null == undefined);
+console.log('Exemplo 2:', null === undefined);
