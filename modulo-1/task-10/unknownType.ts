57ca20616ff7ef48d4650308420f58ef179cfc1c
Stiven-Richardy <stivenrichardy445@gmail.com>
2025-02-11 11:31:00 -0300

    Atualização após a conclusão da Tarefa 10

---
+++ modulo-1/task-10/unknownType.ts
@@ -0,0 +1,28 @@
+// -> Tipo unknown
+let tipoUnknown: unknown;
+
+tipoUnknown = true;
+console.log(tipoUnknown);
+tipoUnknown = 10;
+console.log(tipoUnknown);
+tipoUnknown = 'Corinthians';
+console.log(tipoUnknown);
+
+// -> Erro ao tentar atribuir um valor do tipo unknown a outros tipos
+/*
+let tipoUnknownTipos: unknown;
+
+let injetarUnknownString: string = tipoUnknownTipos;
+let injetarUnknownNumber: number = tipoUnknownTipos;
+let injetarUnknownBoolean: boolean = tipoUnknownTipos;
+let injetarUnknownAny: any[] = tipoUnknownTipos;
+*/
+
+// -> Diferença entre: any vs unknown
+/*
+let diferencaUnknown: unknown;
+let diferencaAny: any;
+
+console.log(diferencaAny.toFixed(2));
+console.log(diferencaUnknown.toFixed(2));
+*/
