6856ced6691ceee02bbffce3077d5ce5687815c3
Stiven-Richardy <stivenrichardy445@gmail.com>
2025-02-12 10:09:12 -0300

    Atualização - Modulo 2 | Atividade 4

---
+++ modulo-02-Fluxos/task-04-while-do/while-do.ts
@@ -0,0 +1,25 @@
+// -> Exemplo 1: Uso do While
+let numeroLoop = 0;
+
+while (numeroLoop <= 10) {
+    console.log(numeroLoop);
+    numeroLoop++;
+}
+
+// -> Exemplo 2: Uso do Do-While
+let numeroLoop2 = 0;
+
+do {
+    console.log(numeroLoop2);
+    numeroLoop2++;
+} while (numeroLoop2 <= 10);
+
+// -> Exemplo3: Uso com arrays
+let numeroLoopi = [1, 2, 3, 4, 5];
+
+let indice = 0;
+
+while (indice < numeroLoopi.length) {
+    console.log(numeroLoopi[indice]);
+    indice++;
+}
