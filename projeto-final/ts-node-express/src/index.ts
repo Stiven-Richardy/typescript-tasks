f3f00516b1c4a37aecf83a3ee1cd85242c5d53e0
Stiven-Richardy <stivenrichardy445@gmail.com>
2025-02-14 11:41:00 -0300

    Atualização - Projeto Final

---
+++ projeto-final/ts-node-express/src/index.ts
@@ -0,0 +1,20 @@
+import express, { Express, Request, Response } from "express";
+import helmet from "helmet";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app: Express = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('<h1>Olá, Mundo!</h1>');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server rodando na porta ${PORT}`);
+});
